f2c565154751a6bbbf418b7fd0540c1a3e3c53b0
Juliana Gaibler <git@juliana.me>
2025-01-14 18:34:00 +0100

    🐛 Fixed broken support link for shortcuts

--- src/components/preferences/Shortcut.svelte
+++ src/components/preferences/Shortcut.svelte
@@ -5,15 +5,17 @@
   import TextField from 'tint/components/TextField.svelte'
 
   let shortcut = $state('-/-')
-  shortcutGet('_execute_browser_action').then((result) => {
+  shortcutGet('_execute_action').then((result) => {
     shortcut = result || ''
   })
+
+  const IS_FIREFOX = navigator.userAgent.includes('Firefox')
 </script>
 
 <div>
   <h2 class="tint--type-ui-bold">{$_('preferences-shortcut-headline')}</h2>
   <p id="locale-description">
-    <LocalizedWithLink id="preferences-shortcut-explanation" url="#" />
+    <LocalizedWithLink id="preferences-shortcut-explanation" url={IS_FIREFOX ? $_('url-changing-shortcuts-firefox') : $_('url-changing-shortcuts-chrome')} />
   </p>
 </div>
 <div class="controls">
